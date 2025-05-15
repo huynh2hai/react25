bb0c85c55b9db3da44ef9c3d6d57892fa1223131
Hai Huynh <hai.huynh@hometogo.com>
2025-05-15 13:08:26 +0700

    init project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {PageWrapper} from "./components/PageWrapper";
+import {Container} from "./components/Container";
+import {Header} from "./components/Header";
+
+export function App() {
+    return (
+        <PageWrapper>
+            <Container>
+                <Header />
+            </Container>
+        </PageWrapper>
+    )
+}
---
+++ src/entry.jsx
@@ -0,0 +1,12 @@
+import { createRoot } from 'react-dom/client'
+import './app.css'
+import {StrictMode} from "react";
+import {App} from "./App";
+
+
+
+createRoot(document.getElementById('app')).render(
+   <StrictMode>
+       <App />
+   </StrictMode>
+);
