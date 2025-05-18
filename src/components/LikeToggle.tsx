5a7ba13d38a4e3cd73397280f82f5c50721ef3cc
Hai Huynh <hai.huynh@hometogo.com>
2025-05-18 12:57:03 +0700

    Handle submit form for add new puppy

--- src/components/LikeToggle.tsx
+++ src/components/LikeToggle.tsx
@@ -1,5 +1,5 @@
 import {type Puppy} from "../types";
-import {Heart} from "lucide-react";
+import {Heart, LoaderCircle} from "lucide-react";
 import {Dispatch, SetStateAction, useState} from "react";
 
 export function LikeToggle({id, liked, setLiked}: {
@@ -7,17 +7,31 @@ export function LikeToggle({id, liked, setLiked}: {
     liked: Puppy["id"][];
     setLiked: Dispatch<SetStateAction<Puppy["id"][]>>;
 }) {
+
+    const [pending, setPending] = useState(false);
+
     return (<button className="group" onClick={() => {
-        if (liked.includes(id)) {
-            setLiked(liked.filter(pupId => pupId !== id));
-        } else {
-            setLiked([...liked, id]);
-        }
-    }}>
-        <Heart className={
-            liked.includes(id)
-                ? "fill-pink-500 stroke-none"
-                : "stroke-slate-200 group-hover:stroke-slate-300"
-        } />
-    </button>);
+            setPending(true);
+            setTimeout(() => {
+                if (liked.includes(id)) {
+                    setLiked(liked.filter(pupId => pupId !== id));
+                } else {
+                    setLiked([...liked, id]);
+                }
+                setPending(false);
+            }, 1500);
+        }}>
+            {pending ? (
+                <LoaderCircle className="animate-spin stroke-slate-300"/>
+            ) : (
+                <Heart
+                    className={
+                        liked.includes(id)
+                            ? "fill-pink-500 stroke-none"
+                            : "stroke-slate-200 group-hover:stroke-slate-300"
+                    }
+                />
+            )}
+        </button>
+    );
 }
--- src/components/NewPuppyForm.tsx
+++ src/components/NewPuppyForm.tsx
@@ -1,5 +1,6 @@
 import {Dispatch, SetStateAction} from "react";
 import {type Puppy} from "../types";
+import {useFormStatus} from "react-dom";
 
 export function NewPuppyForm({puppies, setPuppies}: {
     puppies: Puppy[];
@@ -7,12 +8,14 @@ export function NewPuppyForm({puppies, setPuppies}: {
 }) {
     return (<div className="mt-12 flex items-center justify-between bg-white p-8 shadow ring ring-black/5">
         <form
-            action={(formData: FormData) => {
+            action={async (formData: FormData) => {
+                await new Promise((resolve) => setTimeout(resolve, 1500));
+
                 const newPuppy: Puppy = {
                     id: puppies.length +1,
                     name: formData.get("name") as string,
                     vibe: formData.get("trait") as string,
-                    imagePath: `/images/${puppies.length + 1}.jpg`,
+                    imagePath: `/images/${Math.floor(Math.random() * 16) + 7}.jpg`,
                 };
 
                 setPuppies([...puppies, newPuppy]);
@@ -22,6 +25,7 @@ export function NewPuppyForm({puppies, setPuppies}: {
                 <fieldset className="flex w-full flex-col gap-1">
                     <label htmlFor="name">Name</label>
                     <input
+                        required
                         className="max-w-96 rounded-sm bg-white px-2 py-1 ring ring-black/20 focus:ring-2 focus:ring-cyan-500 focus:outline-none"
                         id="name"
                         type="text"
@@ -31,6 +35,7 @@ export function NewPuppyForm({puppies, setPuppies}: {
                 <fieldset className="flex w-full flex-col gap-1">
                     <label htmlFor="trait">Personality trait</label>
                     <input
+                        required
                         className="max-w-96 rounded-sm bg-white px-2 py-1 ring ring-black/20 focus:ring-2 focus:ring-cyan-500 focus:outline-none"
                         id="trait"
                         type="text"
@@ -50,12 +55,20 @@ export function NewPuppyForm({puppies, setPuppies}: {
                     />
                 </fieldset>
             </div>
-            <button
-                className="mt-4 inline-block rounded bg-cyan-300 px-4 py-2 font-medium text-cyan-900 hover:bg-cyan-200 focus:ring-2 focus:ring-cyan-500 focus:outline-none"
-                type="submit"
-            >
-                Add puppy
-            </button>
+            <SubmitButton />
         </form>
     </div>);
+}
+
+function SubmitButton() {
+    const status = useFormStatus();
+    return (
+        <button
+            className="mt-4 inline-block rounded bg-cyan-300 px-4 py-2 font-medium text-cyan-900 hover:bg-cyan-200 focus:ring-2 focus:ring-cyan-500 focus:outline-none"
+            type="submit"
+            disabled={status.pending}
+        >
+            {status.pending ? `Adding ${status.data.get('name') || 'Puppy'}....` : "Add puppy"}
+        </button>
+    );
 }
